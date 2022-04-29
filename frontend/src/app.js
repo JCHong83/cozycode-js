0b65bc8f4f9a17af88739807912caa37ccad9993
JC Hong <jaechol.hong@outlook.com>
2022-04-29 17:28:41 +0200

    First Commit

---
+++ frontend/src/app.js
@@ -0,0 +1,22 @@
+import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
+import { parseRequestUrl } from './utils.js';
+
+// Here routes are defined as pairs of address and responding 'screen'
+const routes = {
+  '/': HomeScreen,
+  '/product/:id': ProductScreen,
+};
+
+const router = () => {
+  const request = parseRequestUrl;
+  const parseUrl =
+    (request.resource ? `/${request.resource}` : '/') +
+    (request.id ? '/:id' : '') +
+    (request.verb ? `/${request.verb}` : '');
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+  const main = document.getElementById('main-container');
+  main.innerHTML = HomeScreen.render();
+};
+
+window.addEventListener('load', router);
