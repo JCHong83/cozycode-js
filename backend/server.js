32034931f33b9ac761c7b484c556c50b310a8486
JC Hong <jaechol.hong@outlook.com>
2022-05-01 00:46:15 +0200

    products loading from backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express'); // get access to express package
+const cors = require('cors');
 const data = require('./data.js'); // to bring data from data.js file
 const app = express(); // run express that returns our web app
+
+app.use(cors());
 
 app.get('/api/products', (req, res) => {
   res.send(data.products); // for sending data to the client
