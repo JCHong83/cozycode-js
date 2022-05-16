76b60e71fd7bc81b9a9eb2f7dbfec9b309751ef0
JC Hong <jaechol.hong@outlook.com>
2022-05-16 18:06:16 +0200

    product screen api and ui

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ app.get('/api/products', (req, res) => {
   res.send(data.products); // for sending data to the client
 });
 
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found!' });
+  }
+  res.send();
+});
+
 app.listen(5000, () => {
   console.log('serve at http://localhost:5000');
 });
